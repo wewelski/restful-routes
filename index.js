b487f28f2a2978cf281d1b69030e627fd46be75e
wewelski <f.roowell@gmail.com>
2023-06-03 23:51:34 +0800

    Added .redirect method to go back to updated /comments

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/comments/new',(req,res) => {
 app.post('/comments',(req,res) => {
   const { username, comment} = req.body;
   comments.push({ username, comment});
-  res.send('Comment added! Kindly refresh the page to view the update.');
+  res.redirect('/comments');
 })
 
 app.listen(3000, () => {
